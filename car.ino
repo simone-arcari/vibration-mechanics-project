8d6463c90079d2cb330712420befa70130f7b019
Simone Arcari <53971242+simone-arcari@users.noreply.github.com>
2022-10-26 01:15:49 +0200

    Update car.ino

--- car.ino
+++ car.ino
@@ -28,9 +28,10 @@ SoftwareSerial BTserial(RX, TX);
 // Connect the HC-05 TX to Arduino pin 2 RX.
 // Connect the HC-05 RX to Arduino pin 3 TX through a voltage divider.
 
-char receivedFromHC05[CMD_LENGTH + 1] = { 0 };
+bool stop_flag = true;
 uint16_t res;
 uint8_t speed_value = 0;
+char receivedFromHC05[CMD_LENGTH + 1] = { 0 };
 
 void setup() {
   Serial.begin(__BAUD);
@@ -48,35 +49,40 @@ void setup() {
 
 void loop() {
 
-
   if (res = (BTserial.available() >= CMD_LENGTH)) {
     BTserial.readBytes(receivedFromHC05, CMD_LENGTH);
     receivedFromHC05[CMD_LENGTH] = '\0';
 
     if (strcmp(receivedFromHC05, UINT_CMD) == 0) {    // RICORDA: possibile problema sincronizzazione
       BTserial.readBytes(&speed_value,1);             // RICORDA: possibile problema sincronizzazione
-      analogWrite(PIN_TRANSISTOR_PWM, speed_value);   // RICORDA: possibile problema sincronizzazione
-
+      if (stop_flag == false)
+      {
+        analogWrite(PIN_TRANSISTOR_PWM, speed_value);  // accendo il transistor(motore acceso a velocità memorizzata)
+      }
     }
     else if (strcmp(receivedFromHC05, STOP_CMD) == 0) {
       digitalWrite(PIN_RELE, HIGH);  // spengo i rele(marcia avanti)
       analogWrite(PIN_TRANSISTOR_PWM, 0);  // spengo il transistor(motore spento)
       digitalWrite(LED13, LOW);
+      stop_flag = true;
 
     }
     else if (strcmp(receivedFromHC05, GO_ON_CMD) == 0) {
       digitalWrite(PIN_RELE, HIGH);   // spengo i rele(marcia avanti)
       analogWrite(PIN_TRANSISTOR_PWM, speed_value);  // accendo il transistor(motore acceso a velocità memorizzata)
       digitalWrite(LED13, HIGH);
+      stop_flag = false;
 
     }
     else if (strcmp(receivedFromHC05, GO_BACK_CMD) == 0) {
       digitalWrite(PIN_RELE, LOW);  // accendo i rele(marcia indietro)
-      analogWrite(PIN_TRANSISTOR_PWM, HIGH);  // accendo il transistor(motore acceso a velocità memorizzata)
+      analogWrite(PIN_TRANSISTOR_PWM, speed_value);  // accendo il transistor(motore acceso a velocità memorizzata)
       digitalWrite(LED13, HIGH);
+      stop_flag = false;
+      
     }
   
     else  /* pulisco il buffer */
-      for (int i = 0; i < res; i++) BTserial.readBytes(receivedFromHC05, 1);
+      for (uint16_t i = 0; i < res; i++) BTserial.readBytes(receivedFromHC05, 1);
   }
 }
