c9530a3f3f40b8f97d1f29c21c3bf3787ce888e9
Simone Arcari <53971242+simone-arcari@users.noreply.github.com>
2022-10-29 15:40:26 +0200

    Update car.ino

--- car.ino
+++ car.ino
@@ -12,7 +12,7 @@ hfe = 270
 #define RX 2
 #define TX 3
 #define PIN_RELE 4
-#define PIN_TRANSISTOR_PWM 5
+#define PIN_TRANSISTOR 5
 #define LED13 13
 
 #define CMD_LENGTH 4
@@ -28,9 +28,9 @@ SoftwareSerial BTserial(RX, TX);
 // Connect the HC-05 TX to Arduino pin 2 RX.
 // Connect the HC-05 RX to Arduino pin 3 TX through a voltage divider.
 
-bool stop_flag = true;
+//bool stop_flag = true;
 uint16_t res;
-uint8_t speed_value = 0;
+//uint8_t speed_value = 0;
 char receivedFromHC05[CMD_LENGTH + 1] = { 0 };
 
 void setup() {
@@ -39,11 +39,11 @@ void setup() {
 
   pinMode(LED13, OUTPUT);
   pinMode(PIN_RELE, OUTPUT);
-  pinMode(PIN_TRANSISTOR_PWM, OUTPUT);
+  pinMode(PIN_TRANSISTOR, OUTPUT);
 
   digitalWrite(LED13, LOW);
   digitalWrite(PIN_RELE, HIGH);               // spengo i rele(polarità standard motore)
-  analogWrite(PIN_TRANSISTOR_PWM, speed_value);   // spengo il transistor(motore spento)
+  digitalWrite(PIN_TRANSISTOR, LOW);   // spengo il transistor(motore spento)
 
 }
 
@@ -52,33 +52,35 @@ void loop() {
   if (res = (BTserial.available() >= CMD_LENGTH)) {
     BTserial.readBytes(receivedFromHC05, CMD_LENGTH);
     receivedFromHC05[CMD_LENGTH] = '\0';
-
+/*    
     if (strcmp(receivedFromHC05, UINT_CMD) == 0) {    // RICORDA: possibile problema sincronizzazione
       BTserial.readBytes(&speed_value,1);             // RICORDA: possibile problema sincronizzazione
       if (stop_flag == false)
       {
-        analogWrite(PIN_TRANSISTOR_PWM, speed_value);  // accendo il transistor(motore acceso a velocità memorizzata)
+        digitalWrite(PIN_TRANSISTOR, speed_value);  // accendo il transistor
       }
     }
-    else if (strcmp(receivedFromHC05, STOP_CMD) == 0) {
+    else 
+    */
+    if (strcmp(receivedFromHC05, STOP_CMD) == 0) {
       digitalWrite(PIN_RELE, HIGH);  // spengo i rele(marcia avanti)
-      analogWrite(PIN_TRANSISTOR_PWM, 0);  // spengo il transistor(motore spento)
+      digitalWrite(PIN_TRANSISTOR, LOW);  // spengo il transistor(motore spento)
       digitalWrite(LED13, LOW);
-      stop_flag = true;
+      //stop_flag = true;
 
     }
     else if (strcmp(receivedFromHC05, GO_ON_CMD) == 0) {
       digitalWrite(PIN_RELE, HIGH);   // spengo i rele(marcia avanti)
-      analogWrite(PIN_TRANSISTOR_PWM, speed_value);  // accendo il transistor(motore acceso a velocità memorizzata)
+      digitalWrite(PIN_TRANSISTOR, HIGH);  // accendo il transistor
       digitalWrite(LED13, HIGH);
-      stop_flag = false;
+      //stop_flag = false;
 
     }
     else if (strcmp(receivedFromHC05, GO_BACK_CMD) == 0) {
       digitalWrite(PIN_RELE, LOW);  // accendo i rele(marcia indietro)
-      analogWrite(PIN_TRANSISTOR_PWM, speed_value);  // accendo il transistor(motore acceso a velocità memorizzata)
+      digitalWrite(PIN_TRANSISTOR, HIGH);  // accendo il transistor
       digitalWrite(LED13, HIGH);
-      stop_flag = false;
+      //stop_flag = false;
       
     }
   
